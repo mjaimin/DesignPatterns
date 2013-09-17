11797c76ae58a2dc10ded0f1d955e13cb0e2a744
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-09-17 15:04:04 +0530

    Composit DP code Reorganized

--- Structural/Composite/Composite/Main.cpp
+++ Structural/Composite/Composite/Main.cpp
@@ -1,29 +1,82 @@
-// Main.cpp : Defines the entry point for the console application.
-//
+#include <iostream>
+#include <algorithm>
+#include <list>
+using namespace std;
 
-#include "stdafx.h"
-#include "Component.h"
-#include "Composite.h"
-#include "Leaf.h"
+class Component {
+public:
+    virtual void operation() = 0;
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-    Component* root = new Composite();
-    Component* leaf1 = new Leaf();
-    Component* leaf2 = new Leaf();
-    Component* composite = new Composite();
+    virtual void add( Component* child ) { }
+    virtual void remove( Component* child ) { }
+    virtual Component* getChild( int index ) { return NULL; }
+};
 
-    root->Add(leaf1);
-    root->Add(leaf2);
-    root->Add(composite);
+class Leaf : public Component {
+public:
+    virtual void operation() {
+        cout << "Operation by leaf" << endl;
+    }
+};
 
-    root->Operation();
+class Composite : public Component {
+public:
 
-    root->Remove(leaf2);
+    virtual void add( Component* child ) {
+        m_components.push_back( child );
+    }
 
-    //clean up
-    delete root;
+    virtual void remove( Component* child ) {
+        list<Component*>::iterator iter;
+        iter = find( m_components.begin(), m_components.end(), child);
 
-	return 0;
+        if ( m_components.end() != iter ) {
+            m_components.erase( iter );
+        }
+    }
+
+    virtual Component* getChild( int index ) {
+        list<Component*>::iterator iter1, iter2;
+        int i;
+        for ( i = 1, iter1 = m_components.begin(), iter2 = m_components.end();
+            iter1 != iter2; ++iter1, ++i) 
+        {
+            if ( i == index )
+                break;
+        }
+
+        return *iter1;
+    }
+
+    virtual void operation() {
+        cout << "Operation by Composite" << endl;
+        list<Component*>::iterator iter1, iter2;
+        for( iter1 = m_components.begin(), iter2 = m_components.end();
+            iter1 != iter2; ++iter1)
+        {
+            (*iter1)->operation();
+        }
+    }
+
+private:
+    list<Component*> m_components;
+};
+
+int main() {
+    Leaf* leaf1 = new Leaf();
+    Leaf* leaf2 = new Leaf();
+
+    Composite* composite = new Composite();
+    composite->add( leaf1 );
+    composite->add( leaf2 );
+    Composite* composite2 = new Composite();
+    composite2->add( new Leaf() );
+    composite2->add( new Leaf() );
+    composite2->add( new Leaf() );
+    composite2->add( new Leaf() );
+    composite->add( composite2 );
+
+    composite->operation();
+
+
 }
-
