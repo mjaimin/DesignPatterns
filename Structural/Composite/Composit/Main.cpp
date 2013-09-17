f7c676d3e24d57c8bfa00d712d4a5abfe32e24d3
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-09-17 16:51:39 +0530

    Restructured Composite code

---
+++ Structural/Composite/Composit/Main.cpp
@@ -0,0 +1,75 @@
+#include <list>
+#include <iostream>
+#include <string>
+
+class IEmployee
+{
+public:
+    virtual void ShowHappiness() = 0;
+};
+
+class Worker: public IEmployee
+{
+private:
+    std::string name;
+    int happiness;
+
+public:
+    Worker(std::string name, int happiness)
+    {
+        this->name = name;
+        this->happiness = happiness;
+    }
+
+    void ShowHappiness()
+    {
+        std::cout << name <<  " showed happiness level of " << happiness << std::endl;
+    }
+};
+
+class Supervisor: public IEmployee
+{
+private:
+    std::string name;
+    int happiness; 
+    std::list<IEmployee*> subordinate;
+
+public:
+    Supervisor(std::string name, int happiness)
+    {
+        this->name = name;
+        this->happiness = happiness;
+    }
+
+    void ShowHappiness()
+    {
+        std::cout << name << " showed happiness level of " << happiness << std::endl;
+        //show all the subordinate's happiness level
+        for (std::list<IEmployee*>::iterator it = subordinate.begin(); it != subordinate.end(); it++)
+            (*it)->ShowHappiness();
+    }
+
+    void AddSubordinate(IEmployee* employee)
+    {
+        subordinate.push_back(employee);
+    }
+};
+
+
+int main()
+{
+    Worker* a = new Worker("Worker Tom", 5);
+    Supervisor* b = new Supervisor("Supervisor Mary", 6);
+    Supervisor* c = new Supervisor("Supervisor Jerry", 7);
+    Supervisor* d = new Supervisor("Supervisor Bob", 9);
+    Worker* e = new Worker("Worker Jimmy", 8);
+
+    //set up the relationships
+    b->AddSubordinate(a); //Tom works for Mary
+    c->AddSubordinate(b); //Mary works for Jerry
+    c->AddSubordinate(d); //Bob works for Jerry
+    d->AddSubordinate(e); //Jimmy works for Bob
+
+    //Jerry shows his happiness and asks everyone else to do the same
+    c->ShowHappiness();
+}
---
+++ Structural/Composite/README.md
@@ -0,0 +1,28 @@
+##Purpose
+Facilitates the creation of object hierarchies where each object can be treated independently or as a set of nested objects through the same interface.
+
+##Intent
++	Compose objects into tree structures to represent part-whole hierarchies.
++	Composite lets clients treat individual objects and compositions of objects uniformly.
+
+##Participants
+The classes and/or objects participating in this pattern are:
+
++	Component   (DrawingElement)
+
+		-	Declares the interface for objects in the composition.
+		-	Implements default behavior for the interface common to all classes, as appropriate.
+		-	Declares an interface for accessing and managing its child components.
+		-	(optional) Defines an interface for accessing a component's parent in the recursive structure, and implements it if that's appropriate.
++	Leaf   (PrimitiveElement)
+
+		-	Represents leaf objects in the composition. A leaf has no children.
+		-	Defines behavior for primitive objects in the composition.
++	Composite   (CompositeElement)
+
+		-	Defines behavior for components having children.
+		-	Stores child components.
+		-	Implements child-related operations in the Component interface.
++	Client  (CompositeApp)
+
+		-	Mzanipulates objects in the composition through the Component interface.
