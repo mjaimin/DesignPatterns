8a0e722841d72417ee4e1e45941df04e97637902
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-09-18 17:53:12 +0530

    Merged Flyweight-3 code in to single file

--- Structural/Flyweight/FlyWeight-3/Flyweight.cpp
+++ Structural/Flyweight/FlyWeight-3/Flyweight.cpp
@@ -1,6 +1,73 @@
 
-#include "Flyweight.h"
 
+
+
+/*	Flyweight Pattern
+*
+*	This progarm is used to show how flyweight pattern works.
+*	In this program, we assume that there is a weapon store.
+*   We store different kind of guns there. If one kind of gun 
+*   is picked up, it is extrinsic.
+*
+*   @ Wang Qiang  2010 NTU
+*
+*/
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+// Class Gun (Flyweight)
+class Gun{
+public:
+	~Gun();
+	Gun(const char* name);
+	Gun(const Gun& rhs);
+	void Load(int num);
+	void ShowMe();
+	bool operator == (const Gun& rhs);
+	bool operator == (const char* rhs);
+	Gun& operator = (const Gun& rhs);
+private:
+	const char* gName;  // intrinsic
+	int bullet;         // extrinsic
+};
+
+// Class GunFactory (FlyweightFactory)
+class GunFactory{
+public:
+	~GunFactory();
+	GunFactory();
+	Gun* GetGun(const char* name);
+private:
+	list<Gun*> gunPool;
+};
+
+// Class GunCheck
+class GunCheck{
+public:
+	~GunCheck();
+	GunCheck(int n, const char* name);
+	int GetNum();
+	void AddNum(int n);
+	bool operator ==(const char* rhs);
+private:
+	int no;
+	const char* gName;
+};
+
+// Class WeaponStore (Client)
+class WeaponStore{
+public:
+	~WeaponStore();
+	WeaponStore();
+	Gun* AddGun(const char* name, int num);
+	Gun* PickUp(const char* name, int bullet);
+private:
+	list<GunCheck> gunList; 
+	GunFactory* myFactory;
+};
 // Class Gun
 Gun::~Gun(){}
 
@@ -115,4 +182,24 @@ Gun* WeaponStore::PickUp(const char *name, int bullet){
 	Gun* newGun = new Gun(*tGun);
 	newGun->Load(bullet);
 	return newGun;
+}
+
+
+#include <iostream>
+#include "Flyweight.h"
+using namespace std;
+
+void main(){
+	WeaponStore myStore = WeaponStore();
+	Gun* myGun1 = myStore.AddGun("M4A1",10);
+	Gun* myGun2 = myStore.AddGun("AK47",15);
+	Gun* myGun3 = myStore.PickUp("M4A1",70);
+	Gun* myGun4 = myStore.PickUp("AK47",90);
+	myGun1->ShowMe();  // Guns in store
+	myGun2->ShowMe();  // Guns in store
+	myGun3->ShowMe();  // Particular gun I picked up
+	myGun4->ShowMe();  // Particular gun I picked up
+	while(1){
+		;
+	}
 }
