c2cfd12501a847459c259425bd0ebeea5653e421
unknown <modijaim@PC-A48936.patni-emcase.com>
2013-09-26 17:01:28 +0530

    Added New Visitor Example

--- Behavioural/Visitor/Visitor-3/Main.cpp
+++ Behavioural/Visitor/Visitor-3/Main.cpp
@@ -1,48 +1,19 @@
 #include <iostream>
 #include <string>
-class IVisitor{
-    public:
-void IVisitor::Visit();
-}
+
+class IVisitor;
 
 class IElement
 {
 public:
    std::string Name;
-   virtual void Accept(IVisitor v) = 0;
+   virtual void Accept(IVisitor *v) = 0;
 };
 
-class Household : public IElement
-{
-public: Household(std::string name)
-   {
-      Name = name;
-   }
-
-   void Accept(IVisitor* v)
-   {
-      v->Visit(this);
-   }
-};
-
-class BusinessEntity : public IElement
+class IVisitor
 {
 public:
-   BusinessEntity(std::string name)
-   {
-      Name = name;
-   }
-
-   void Accept(IVisitor v)
-   {
-      v->Visit(this);
-   }
-};
-
-public IVisitor
-{
-public:
-   virtual void Visit(IElement * e) = 0;
+   virtual void Visit(IElement *e) = 0;
 };
 
 class SantaClaus : public IVisitor
@@ -63,27 +34,58 @@ public:
    }
 };
 
+
+class Household : public IElement
+{
+private:
+   Household() {}
+public: Household(std::string name)
+   {
+      Name = name;
+   }
+
+   void Accept(IVisitor *v)
+   {
+      v->Visit(this);
+   }
+};
+
+class BusinessEntity : public IElement
+{
+private:
+   BusinessEntity() {}
+public:
+   BusinessEntity(std::string name)
+   {
+      Name = name;
+   }
+
+   void Accept(IVisitor *v)
+   {
+      v->Visit(this);
+   }
+};
+
 int main()
 {
    //first set up the structure
    IElement *household1      = new Household("The Adams Family");
    IElement *household2      = new Household("The Jones Family");
-   IVisitor *BusinessEntity1 = new BusinessEntity("The Oatmeal Bakery");
-   IVisitor *BusinessEntity2 = new BusinessEntity("The Ice Cream Shop");
+   IElement *BusinessEntity1 = new BusinessEntity("The Oatmeal Bakery");
+   IElement *BusinessEntity2 = new BusinessEntity("The Ice Cream Shop");
 
    //use one visitor, or logic
    IVisitor *visitor = new SantaClaus();
 
-   household1.Accept(visitor);
-   household2.Accept(visitor);
-   BusinessEntity1.Accept(visitor);
-   BusinessEntity2.Accept(visitor);
+   household1->Accept(visitor);
+   household2->Accept(visitor);
+   BusinessEntity1->Accept(visitor);
+   BusinessEntity2->Accept(visitor);
 
-   i.Accept(visitor);          //apply the logic to the element
    //use another visitor, or logic
    visitor = new MailCarrier();
-   household1.Accept(visitor);
-   household2.Accept(visitor);
-   BusinessEntity1.Accept(visitor);
-   BusinessEntity2.Accept(visitor);
+   household1->Accept(visitor);
+   household2->Accept(visitor);
+   BusinessEntity1->Accept(visitor);
+   BusinessEntity2->Accept(visitor);
 }
