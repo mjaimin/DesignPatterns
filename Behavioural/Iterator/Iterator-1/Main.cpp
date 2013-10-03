19c6d9977eca6db2c23ff037c87b0499d97eef1e
m.jaimin@gmail.com <m.jaimin@gmail.com>
2013-10-03 14:49:33 +0530

    Converted code to classic code

--- Behavioural/Iterator/Iterator-1/Main.cpp
+++ Behavioural/Iterator/Iterator-1/Main.cpp
@@ -7,7 +7,7 @@ class IIterator
 public:
 
    virtual std::string FirstItem()   = 0;
-   virtual std::string NextItem()    = 0;
+   virtual void NextItem()           = 0;
    virtual std::string CurrentItem() = 0;
    virtual bool IsDone()             = 0;
 };
@@ -16,105 +16,70 @@ class IAggregate
 {
 public:
 
-   virtual IIterator *GetIterator() = 0;
-   virtual std::string& operator[](int itemIndex) = 0;
+   virtual IIterator *GetIterator()            = 0;
+   virtual std::string& getItem(int itemIndex) = 0;
+   virtual void AddValue(std::string value)    = 0;
    virtual int Count() = 0;
 };
 
-class MyIterator : public IIterator
+class ConcreteIterator : public IIterator
 {
 private:
    IAggregate *m_aggregate;
    int        m_currentIndex;
 
 public:
-   MyIterator(IAggregate *aggregate) : m_currentIndex(0), m_aggregate(aggregate) { }
-
-   std::string FirstItem()
-   {
-      m_currentIndex = 0;
-      return (*m_aggregate)[m_currentIndex];
-   }
-
-   std::string NextItem()
-   {
-      m_currentIndex += 1;
-
-      if (IsDone() == false)
-      {
-         return (*m_aggregate)[m_currentIndex];
-      }
-      else
-      {
-         return "";
-      }
-   }
-
-   std::string CurrentItem()
-   { return (*m_aggregate)[m_currentIndex]; }
-
-   bool IsDone()
-   {
-      if (m_currentIndex < m_aggregate->Count())
-      {
-         return false;
-      }
-      return true;
-   }
+   ConcreteIterator(IAggregate *aggregate) : m_currentIndex(0), m_aggregate(aggregate) { }
+   std::string FirstItem() { return m_aggregate->getItem(0); }
+   void NextItem() { m_currentIndex += 1; }
+   std::string CurrentItem() { return m_aggregate->getItem(m_currentIndex); }
+   bool IsDone() { return (m_currentIndex < m_aggregate->Count()) ? false : true; }
 };
 
-class MyAggregate : public IAggregate
+class ConcreteAggregate : public IAggregate
 {
 private:
    std::vector<std::string> m_Vect;
 
 public:
-   MyAggregate(void) { }
+   ConcreteAggregate(void) { }
 
 //Helper function to populate the collection
-   void AddValue(std::string value)
-   {
-      m_Vect.push_back(value);
-   }
+   void AddValue(std::string value) { m_Vect.push_back(value); }
 
    IIterator *GetIterator()
    {
-      IIterator *iter = new MyIterator(this);
+      IIterator *iter = new ConcreteIterator(this);
 
       return iter;
    }
 
-   std::string& operator[](int itemIndex)
-   {
-      return m_Vect[itemIndex];
-   }
+   std::string& getItem(int itemIndex)   { return m_Vect[itemIndex]; }
 
-   int Count()
-   {
-      return m_Vect.size();
-   }
+   int Count()   { return m_Vect.size(); }
 };
 
 int main()
 {
-   MyAggregate aggr;
+   IAggregate *aggr     = new ConcreteAggregate;
+   IIterator  *iterator = new ConcreteIterator(aggr);
 
-   aggr.AddValue("1");
-   aggr.AddValue("2");
-   aggr.AddValue("3");
-   aggr.AddValue("4");
-   aggr.AddValue("5");
-   aggr.AddValue("6");
-   aggr.AddValue("7");
-   aggr.AddValue("8");
-   aggr.AddValue("9");
-   aggr.AddValue("10");
+   aggr->AddValue("2");
+   aggr->AddValue("1");
+   aggr->AddValue("3");
+   aggr->AddValue("4");
+   aggr->AddValue("5");
+   aggr->AddValue("6");
+   aggr->AddValue("7");
+   aggr->AddValue("8");
+   aggr->AddValue("9");
+   aggr->AddValue("10");
 
-   IIterator *iter = aggr.GetIterator();
+   IIterator *iter = aggr->GetIterator();
 
-   for (std::string s = iter->FirstItem(); iter->IsDone() == false; s = iter->NextItem())
+   for (/*iter->FirstItem()*/; iter->IsDone() == false; iter->NextItem())
    {
-      std::cout << s << std::endl;
+      std::cout << iter->CurrentItem() << std::endl;
    }
 
    return 0;
