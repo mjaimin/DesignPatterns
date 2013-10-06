e1027aef2197679c52a96f822632affb2df42609
Jaimin Arunkumar Modi <m.jaimin@gmail.com>
2013-10-07 01:12:40 +0530

    Memento code modified

--- Behavioural/Memento/Main.cpp
+++ Behavioural/Memento/Main.cpp
@@ -8,27 +8,17 @@ class Memento
 private: std::string state;
 
 public:
-   std::string GetState()
-   {
-      return state;
-   }
+   std::string GetState() { return state; }
 
-   void SetState(std::string state)
-   {
-      this->state = state;
-   }
+   void SetState(std::string state) { this->state = state; }
 };
 
 //the object that we want to save and restore, such as a check point in an application
 class Originator
 {
 private: std::string state;
-
-
-   //for restoring the state
 public:
-   //for saving the state
-   Memento *newMemento()
+   Memento *createMemento()
    {
       Memento *m = new Memento;
 
@@ -36,22 +26,10 @@ public:
       return m;
    }
 
-   void SetMemento(Memento *m)
-   {
-      state = m->GetState();
-   }
-
+   void SetMemento(Memento *m) { state = m->GetState(); }
    //change the state of the Originator
-   void SetState(std::string state)
-   {
-      this->state = state;
-   }
-
-   //show the state of the Originator
-   void ShowState()
-   {
-      std::cout << state << std::endl;
-   }
+   void SetState(std::string state) { this->state = state; }
+   void ShowState() { std::cout << state << std::endl; }
 };
 
 //object for the client to access
@@ -65,7 +43,7 @@ private:
 public:
    void SaveState(Originator *orig)
    {
-      mementoList.push_back(orig->newMemento());
+      mementoList.push_back(orig->createMemento());
    }
 
    //restore state of the originator
@@ -79,32 +57,32 @@ int main()
 {
    //Originator has to be pointer type or passed as  reference here, because caretacker works on orintor
    // originator handles responsibility of transfering state between Caretaker and Program at any time Originator handles value of momento of requested state
-   Originator orig;
-   Caretaker  caretaker;
+   Originator *orig = new Originator ;
+   Caretaker  *caretaker = new Caretaker ;
 
    // Here "state0" is std::string but this is the object whose state hase to be preserved/retrieve by caretaker using Originator
    // so imagin "state0" as templet<T> object trough out the program
-   orig.SetState("state0");
-   caretaker.SaveState(&orig);      //save state of the originator
-   orig.ShowState();
+   orig->SetState("state0");
+   caretaker->SaveState(orig);      //save state of the originator
+   orig->ShowState();
 
-   orig.SetState("state1");
-   caretaker.SaveState(&orig);      //save state of the originator
-   orig.ShowState();
+   orig->SetState("state1");
+   caretaker->SaveState(orig);      //save state of the originator
+   orig->ShowState();
 
-   orig.SetState("state2");
-   caretaker.SaveState(&orig);      //save state of the originator
-   orig.ShowState();
+   orig->SetState("state2");
+   caretaker->SaveState(orig);      //save state of the originator
+   orig->ShowState();
 
-   orig.SetState("state3");
-   caretaker.SaveState(&orig);      //save state of the originator
-   orig.ShowState();
+   orig->SetState("state3");
+   caretaker->SaveState(orig);      //save state of the originator
+   orig->ShowState();
 
-   orig.SetState("state4");
-   caretaker.SaveState(&orig);      //save state of the originator
-   orig.ShowState();
+   orig->SetState("state4");
+   caretaker->SaveState(orig);      //save state of the originator
+   orig->ShowState();
 
    //restore state of the originator
-   caretaker.RestoreState(&orig, 2);
-   orig.ShowState();      //shows state0
+   caretaker->RestoreState(orig, 2);
+   orig->ShowState();      //shows state0
 }
