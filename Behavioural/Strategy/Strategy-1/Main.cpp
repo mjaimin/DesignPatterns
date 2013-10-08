baaadae5632f58b846e9a87830b8b4187a683b92
Jaimin Modi <m.jaimin@gmail.com>
2013-10-08 12:27:54 +0530

    Strategy pattern code added

--- Behavioural/Strategy/Strategy-1/Main.cpp
+++ Behavioural/Strategy/Strategy-1/Main.cpp
@@ -1,28 +1,70 @@
 #include <iostream>
-#include "sortedList.h"
-#include "quickSort.h"
-#include "mergeSort.h"
+#include <list>
 
 using namespace std;
 
+class sortStrategy
+{
+public:
+   virtual void sortFunction(std::list<int>&) = 0;
+};
+
+class sortedList
+{
+private:
+   std::list<int> m_ListToSort;
+   sortStrategy   *m_sortStrategy;
+public:
+   sortedList() : m_sortStrategy(0) { cout << "There is no intial strategy.\n"; }
+   void sortFunction( )
+   {
+      m_sortStrategy->sortFunction(m_ListToSort);
+   }
+
+   void setSortStrategy(sortStrategy *strategy)
+   {
+      std::cout << "Setting a new strategy...\n";
+
+      m_sortStrategy = strategy;
+   }
+
+   void addNumberToList(int value)
+   {
+      // Logic for adding number goes here.
+      m_ListToSort.push_back(value);
+   }
+};
+
+class mergeSort : public sortStrategy
+{
+public:
+   void sortFunction(std::list<int>& listToSort)
+   {
+      cout << "Current Strategy is MergeSort.\n";
+   }
+};
+
+class quickSort : public sortStrategy
+{
+public:
+   void sortFunction(std::list<int>& listToSort)
+   {
+      cout << "Current Strategy is QuickSort.\n";
+   }
+};
 int main()
 {
-    sortedList* studentRecords = new sortedList();
-    
-    studentRecords->addNumberToList(1);
-    studentRecords->addNumberToList(5);
-    studentRecords->addNumberToList(9);
+   sortedList *studentRecords = new sortedList();
 
-    sortStrategy* qs = new quickSort();
-    studentRecords->setSortStrategy(qs);
-    studentRecords->sortFunction();
+   studentRecords->addNumberToList(1);
+   studentRecords->addNumberToList(5);
+   studentRecords->addNumberToList(9);
 
-    sortStrategy* ms = new mergeSort();
-    studentRecords->setSortStrategy(ms);
-    studentRecords->sortFunction();
+   sortStrategy *qs = new quickSort();
+   studentRecords->setSortStrategy(qs);
+   studentRecords->sortFunction();
 
-    delete qs;
-    delete ms;    
-
-    return 0;
+   sortStrategy *ms = new mergeSort();
+   studentRecords->setSortStrategy(ms);
+   studentRecords->sortFunction();
 }
