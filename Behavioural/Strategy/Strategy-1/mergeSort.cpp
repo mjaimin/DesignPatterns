8d3dd17cac1abfd81b1a79dd4bbc667ea51fb926
Jaimin Modi <m.jaimin@gmail.com>
2013-10-08 12:29:47 +0530

    Strategy pattern code restructured

--- Behavioural/Strategy/Strategy-1/mergeSort.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "mergeSort.h"
-
-using namespace std;
-
-void mergeSort::sortFunction( std::list<int> &listToSort )
-{
-    cout<<"Current Strategy is MergeSort.\n";
-}
--- Behavioural/Strategy/Strategy-1/quickSort.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "quickSort.h"
-
-using namespace std;
-
-void quickSort::sortFunction( std::list<int> &listToSort )
-{
-    cout<<"Current Strategy is QuickSort.\n";
-}
