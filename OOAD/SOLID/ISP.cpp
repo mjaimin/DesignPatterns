6592cc263921a1240f25ad8f39d6fdaf232d6141
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-04-25 12:03:22 +0530

    ISp example added

---
+++ OOAD/SOLID/ISP.cpp
@@ -0,0 +1,51 @@
+﻿class IDoor
+{
+    bool IsDoorOpen;
+    public:
+    bool getDoorStatus(){ return IsDoorOpen;}
+    void setDoorStatus( int status ){ IsDoorOpen = status;}
+
+    virtual void Lock() = 0;
+    virtual void Unlock() = 0;
+};
+class ITimerFunction
+{
+    public:
+	virtual void TimerFunction() = 0; 
+};
+
+
+class SimpleDoor : public IDoor
+{
+    public:
+	void Lock()
+	{
+	    // method implementation goes here
+	}
+
+	void Unlock()
+	{
+	    // method implementation goes here
+	}
+};
+
+class AdvancedDoor : public IDoor, public ITimerFunction
+{
+    public:
+
+	void Lock()
+	{
+	    // Method implementation goes here...
+	}
+
+	void Unlock()
+	{
+	    // Method implementation goes here...
+	}
+
+	void TimerFunction()
+	{
+	}
+};
+
+int main(){}
