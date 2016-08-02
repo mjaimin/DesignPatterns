7bdc5f320f49f008eebe711042902850aba6098f
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-08-02 19:54:32 +0530

    classic factory pattern changed

--- Creational/Factory/Classic/Main.cpp
+++ Creational/Factory/Classic/Main.cpp
@@ -1,60 +1,49 @@
 #include <iostream>
+// Factory creates and returns product object
+enum PRODUCTCODE { CONCRETEPRODUCT1,CONCRETEPRODUCT2};
 using namespace std;
 class Product
 {
-public:
-   virtual ~Product() {}
-protected:
-   Product() { }
+    public:
+	virtual void Action(void)=0;
 };
 
-class ConcreteProduct : public Product
+class ConcreteProduct1 : public Product
 {
-public:
-   ConcreteProduct()
-   {
-      cout << "ConcreteProduct....." << endl;
-   }
-
-   ~ConcreteProduct()
-   {
-      cout << "~ConcreteProduct....." << endl;
-   }
+    public:
+	void Action(void) { cout << "ConcreteProduct1....." << endl; }
 };
 
+class ConcreteProduct2 : public Product
+{
+    public:
+	void Action(void) { cout << "ConcreteProduct2....." << endl; }
+};
 
 class Factory
 {
-public:
-   ~Factory() {}
-   virtual Product *CreateProduct() {}
-protected:
-   Factory() { }
+    public:
+	virtual Product *CreateProduct(PRODUCTCODE code) =0;
 };
 
 class ConcreteFactory : public Factory
 {
-public:
-   ConcreteFactory()
-   {
-      cout << "ConcreteFactory...." << endl;
-   }
-
-   ~ConcreteFactory()
-   {
-      cout << "~ConcreteFactory...." << endl;
-   }
-
-   Product *CreateProduct()
-   {
-      return new ConcreteProduct;
-   }
+    public:
+// Factory creates and returns product object
+	Product *CreateProduct( PRODUCTCODE code ) 
+	{
+	    if ( code = CONCRETEPRODUCT1 )
+		return new ConcreteProduct1; 
+	    else if ( code = CONCRETEPRODUCT2 )
+		return new ConcreteProduct2; 
+	}
 };
 
 int main(int argc, char *argv[])
 {
-   Factory *fac = new  ConcreteFactory();
-   Product *p   = fac->CreateProduct();
+    Factory *f = new  ConcreteFactory();
+    Product *p   = f->CreateProduct( CONCRETEPRODUCT2 );
+    p->Action();
 
-   return 0;
+    return 0;
 }
