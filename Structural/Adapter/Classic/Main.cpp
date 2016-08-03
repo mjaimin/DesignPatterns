9b202954e84b46fbef2d0caa1fd41188e578e681
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-08-03 18:33:19 +0530

    commented adapter

--- Structural/Adapter/Classic/Main.cpp
+++ Structural/Adapter/Classic/Main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-
+//Adaptee is like electric socket on wall
 class Adaptee {
     public:
 	void SpecificRequest() { cout << "I do work specific way..." << endl; }
 };
-
+//Target is like different kind of plug not supported by wall socket
 class Target {
     public:
 	virtual void newRequest() { cout << "Target's new request..." << endl; }
 };
-
+//Plug adapter
 class Adapter : public Target, private Adaptee {
     private:
 	Adaptee *adaptee;
