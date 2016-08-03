2c606d222c67d1ce36ebcc58074b22371b7df84c
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-08-03 18:26:35 +0530

    classic adapter added

---
+++ Structural/Adapter/Classic/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Target {
+public:
+   virtual void newRequest() { cout << "Target's new request..." << endl; }
+};
+
+class Adaptee {
+public:
+   void SpecificRequest() { cout << "I do work specific way..." << endl; }
+};
+
+class Adapter : public Target, private Adaptee {
+private:
+   Adaptee *adaptee;
+public:
+   Adapter(Adaptee *obj) : adaptee(obj) {}
+   void newRequest() { adaptee->SpecificRequest(); }
+};
+
+int main()
+{
+   Adaptee *adaptee = new Adaptee();
+   Target  *target = new Adapter(adaptee);
+
+   target->newRequest();
+
+   delete target;
+   delete adaptee;
+
+   return 0;
+}
