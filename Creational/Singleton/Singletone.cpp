f372d76f84e0604d505a1d11e017368dfa1359b6
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-08-03 17:52:03 +0530

    beautified

--- Creational/Singleton/Singletone.cpp
+++ Creational/Singleton/Singletone.cpp
@@ -1,31 +1,40 @@
 class Singleton {
-public:
-    static Singleton* getInstance();
-protected:
-    Singleton();
 private:
-    static Singleton* instance;
+   static Singleton *instance;
+public:
+   static Singleton *getInstance();
+
+   Singleton();
 };
 
-Singleton* Singleton::instance = 0;
+Singleton *Singleton::instance = 0;
 
-Singleton::Singleton() {}
-
-Singleton* Singleton::getInstance () {
-    if ( NULL == instance) //synchronized lock only activeted when instance is NULL, first time
-    {
-    //aquire lock()//this lock ensures only one thread enters beyond this point
-    if ( NULL == instance) 
-        instance = new Singleton;
-    //release lock()
-    }
-    return Singleton::instance;
+Singleton::Singleton()
+{
 }
 
-int main () {
-    Singleton* var1 = Singleton::getInstance();
-    Singleton* var2 = Singleton::getInstance();
-    std::cout << var1 << std::endl;
-    std::cout << var2 << std::endl;
-    return 0;
+
+Singleton *Singleton::getInstance()
+{
+   if (NULL == instance)   //synchronized lock only activeted when instance is NULL, first time
+   {
+      //aquire lock()//this lock ensures only one thread enters beyond this point
+      if (NULL == instance)
+      {
+         instance = new Singleton;
+      }
+      //release lock()
+   }
+   return Singleton::instance;
+}
+
+
+int main()
+{
+   Singleton *var1 = Singleton::getInstance();
+   Singleton *var2 = Singleton::getInstance();
+   std::cout << var1 << std::endl;
+   std::cout << var2 << std::endl;
+
+   return 0;
 }
