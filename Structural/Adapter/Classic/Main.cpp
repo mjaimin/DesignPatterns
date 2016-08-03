0d1f81abdef0f6d76150cdc7757c0571352f16b9
m.jaimin@gmail.com <m.jaimin@gmail.com>
2016-08-03 18:30:02 +0530

    beautified

--- Structural/Adapter/Classic/Main.cpp
+++ Structural/Adapter/Classic/Main.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 using namespace std;
 
-class Target {
-public:
-   virtual void newRequest() { cout << "Target's new request..." << endl; }
+class Adaptee {
+    public:
+	void SpecificRequest() { cout << "I do work specific way..." << endl; }
 };
 
-class Adaptee {
-public:
-   void SpecificRequest() { cout << "I do work specific way..." << endl; }
+class Target {
+    public:
+	virtual void newRequest() { cout << "Target's new request..." << endl; }
 };
 
 class Adapter : public Target, private Adaptee {
-private:
-   Adaptee *adaptee;
-public:
-   Adapter(Adaptee *obj) : adaptee(obj) {}
-   void newRequest() { adaptee->SpecificRequest(); }
+    private:
+	Adaptee *adaptee;
+    public:
+	Adapter(Adaptee *obj) : adaptee(obj) {}
+	void newRequest() { adaptee->SpecificRequest(); }
 };
 
 int main()
 {
-   Adaptee *adaptee = new Adaptee();
-   Target  *target  = new Adapter(adaptee);
+    Adaptee *adaptee = new Adaptee();
+    Target  *target  = new Adapter(adaptee);
 
-   target->newRequest();
+    target->newRequest();
 
-   delete target;
-   delete adaptee;
+    delete target;
+    delete adaptee;
 
-   return 0;
+    return 0;
 }
