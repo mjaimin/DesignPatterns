4398a25b21730469636e4f2661ca6b3fd857061b
Jaimin Modi <m.jaimin@gmail.com>
2020-04-07 10:20:36 +0530

    Restructured the Code

--- Behavioural/ChainOfResponsibility/ChainOfResponsibility-2/GumballEMailHandler.hpp
+++
@@ -1,9 +0,0 @@
-#include "../../Standard.h"
-
-#include "Handler.hpp"
-#include "LastHandler.hpp"
-#include "NewLocationHandler.hpp"
-#include "ComplaintHandler.hpp"
-#include "FanHandler.hpp"
-#include "SpamHandler.hpp"
-#include "Client.hpp"
