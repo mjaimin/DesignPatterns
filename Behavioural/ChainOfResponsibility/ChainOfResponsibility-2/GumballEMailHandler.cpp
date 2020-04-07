20f0a2507ed79f2a683e23d549cac2d0d4458385
Jaimin Modi <m.jaimin@gmail.com>
2020-04-07 10:19:49 +0530

    Restructured the Code

--- Behavioural/ChainOfResponsibility/ChainOfResponsibility-2/GumballEMailHandler.cpp
+++ Behavioural/ChainOfResponsibility/ChainOfResponsibility-2/GumballEMailHandler.cpp
@@ -1,7 +1,9 @@
-#include "GumballEMailHandler.hpp"
-
-using namespace HeadFirstDesignPatterns::ChainOfResponsibility::GumballEMailHandler;
-
+#include <cctype>
+#include <iostream>
+#include <list>
+#include <memory> 
+#include <algorithm> 
+using namespace std;
 const char* email [] = {
 	"You guys really razz my berries, your gumball machines are totally hip",
 	"Cruisin for a bruisin? My kids lost their money trying to win gumballs!", 
@@ -22,9 +24,224 @@ const char* email [] = {
 	"We want gumball machines in all our stores!, sincerly Starbuzz"
 };
 
+class Handler {
+
+	private: mutable std::list< std::string > _requests;
+	private: const Handler* _successor;
+
+	private: Handler( const Handler& ); // Disable copy constructor
+	private: void operator=( const Handler& ); // Disable assignment operator
+
+	protected: explicit Handler( const Handler* successor = 0 ) :
+		   _successor( successor ) {
+		   }
+	public: virtual ~Handler() {
+		}
+	public: virtual void handleRequest( std::string request ) const {
+			if( _successor ) {
+				_successor->handleRequest( request );
+			}
+		}
+	protected: virtual bool canHandleRequest( const std::string& request ) const {
+			   _requests.push_back( request );
+			   return true;
+		   }
+	public: virtual void print() const {
+			std::cout << " count=" << _requests.size() << std::endl;
+			std::list< std::string >::const_iterator iterator = _requests.begin();
+			while( iterator != _requests.end() ) {
+				std::cout << " - " << ( *iterator ).c_str() << std::endl;
+				iterator++;
+			}
+			if( _successor ) {
+				_successor->print();
+			}
+		}
+};
+
+class ComplaintHandler : public Handler {
+
+	public: 
+		explicit ComplaintHandler( const Handler* successor = 0 ) : Handler( successor ) { }
+	public: 
+		void handleRequest( std::string request ) const 
+		{
+			std::transform( request.begin(), request.end(), request.begin(), ::tolower );
+
+			if( canHandleRequest( request ) == true ) {
+				Handler::canHandleRequest( request );
+			} else {
+				Handler::handleRequest( request );
+			}
+		}
+	public: 
+		bool canHandleRequest( const std::string& request ) const 
+		{
+			bool value = false;
+
+			if( std::string::npos != request.find( "bruisin" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "frosted" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "no where" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "rattle my cage" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "tale nightingale" ) ) {
+				value = true;
+			}
+			return value;
+		}
+	public: 
+		void print() const {
+			std::cout << std::endl << "Hate mail, send to Legal:";
+			Handler::print();
+		}
+};
+
+class FanHandler : public Handler {
+
+	public: explicit FanHandler( const Handler* successor = 0 ) :
+		Handler( successor ) {
+		}
+	public: void handleRequest( std::string request ) const {
+			std::transform( request.begin(), request.end(), request.begin(), ::tolower );
+
+			if( canHandleRequest( request ) == true ) {
+				Handler::canHandleRequest( request );
+			} else {
+				Handler::handleRequest( request );
+			}
+		}
+	public: bool canHandleRequest( const std::string& request ) const {
+			bool value = false;
+
+			if( std::string::npos != request.find( "razz" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "hip" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "radioactive" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "blast" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "boss" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "orbit" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "kick" ) ) {
+				value = true;
+			}
+			return value;
+		}
+	public: void print() const {
+			std:: cout << std::endl << "Fan mail, send to CEO:";
+			Handler::print();
+		}
+};
+
+class NewLocationHandler : public Handler {
+
+	public: explicit NewLocationHandler( const Handler* successor = 0 ) : 
+		Handler( successor ) {
+		}
+	public: void handleRequest( std::string request ) const {
+			std::transform( request.begin(), request.end(), request.begin(), ::tolower );
+
+			if( canHandleRequest( request ) == true ) {
+				Handler::canHandleRequest( request );
+			} else {
+				Handler::handleRequest( request );
+			}
+		}
+	public: bool canHandleRequest( const std::string& request ) const {
+			bool value = false;
+			if( std::string::npos != request.find( "store" ) ) {
+				value = true;
+			}
+			return value;
+		}
+	public: void print() const {
+			std::cout << std::endl << "Request mail, send to Business Development:";
+			Handler::print();
+		}
+};
+
+class SpamHandler : public Handler {
+	public: explicit SpamHandler( const Handler* successor = 0 ) :
+		Handler( successor ) {
+		}
+	public: void handleRequest( std::string request ) const {
+			std::transform( request.begin(), request.end(), request.begin(), ::tolower );
+
+			if( canHandleRequest( request ) == true ) {
+				Handler::canHandleRequest( request );
+			} else {
+				Handler::handleRequest( request );
+			}
+		}
+	public: bool canHandleRequest( const std::string& request ) const {
+			bool value = false;
+
+			if( std::string::npos != request.find( "bread" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "buzzin" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "cuzzin" ) ) {
+				value = true;
+			} else if( std::string::npos != request.find( "eyeball" ) ) {
+				value = true;
+			}
+			return value;
+		}
+	public: void print() const {
+			std::cout << std::endl << "Spam mail, send to shredder:";
+			Handler::print();
+		}
+};
+
+class LastHandler : public Handler {
+
+	public: explicit LastHandler( const Handler* successor = 0 ) : 
+		Handler( successor ) {
+		}
+	public: void handleRequest( std::string request ) const {
+			Handler::canHandleRequest( request );
+		}
+	public: void print() const {
+			std:: cout << std::endl << "Unclassified, send to AI team:";
+			Handler::print();
+		}
+};
+
+class Client {
+
+	private: std::unique_ptr< Handler > _lastHandler;
+	private: std::unique_ptr< Handler > _newHandler;
+	private: std::unique_ptr< Handler > _hateHandler;
+	private: std::unique_ptr< Handler > _fanHandler;
+	private: std::unique_ptr< Handler > _spamHandler;
+
+	private: Client( const Client& ); // Disable copy constructor
+	private: void operator=( const Client& ); // Disable assignment operator
+
+	public: Client() :
+		_lastHandler( new LastHandler() ), 
+		_newHandler(  new NewLocationHandler( _lastHandler.get() ) ),
+		_hateHandler( new ComplaintHandler( _newHandler.get() ) ),
+		_fanHandler(  new FanHandler( _hateHandler.get() ) ),
+		_spamHandler( new SpamHandler ( _fanHandler.get() ) ) {
+		}
+	public: void handleRequest( std::string request ) const {
+			_spamHandler->handleRequest( request );
+		}
+	public: void print() const {
+			_spamHandler->print();
+		}
+};
+
 int main( int argc, char* argv[] ) {
 
-	std::auto_ptr< Client > client( new Client() );
+	std::unique_ptr< Client > client( new Client() );
 
 	for( int i = 0; i < sizeof( email ) / sizeof( char* ); i++ ) {
 		client->handleRequest( email[i] );
--- Behavioural/ChainOfResponsibility/ChainOfResponsibility-2/GumballEMailHandler.hpp
+++ Behavioural/ChainOfResponsibility/ChainOfResponsibility-2/GumballEMailHandler.hpp
@@ -1,6 +1,3 @@
-#ifndef	_HFDP_CPP_CHAIN_OF_RESPONSIBILITY_GUMBALL_EMAIL_HANDLER_HPP_
-#define _HFDP_CPP_CHAIN_OF_RESPONSIBILITY_GUMBALL_EMAIL_HANDLER_HPP_
-
 #include "../../Standard.h"
 
 #include "Handler.hpp"
@@ -10,5 +7,3 @@
 #include "FanHandler.hpp"
 #include "SpamHandler.hpp"
 #include "Client.hpp"
-
-#endif
