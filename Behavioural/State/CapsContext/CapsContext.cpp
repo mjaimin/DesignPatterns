9d6a4caa6b41e48758d022a848f811849eec50f5
Jaimin Modi <m.jaimin@gmail.com>
2020-05-03 21:40:36 +0530

    Unique pointer example

---
+++ Behavioural/State/CapsContext/CapsContext.cpp
@@ -0,0 +1,75 @@
+// State design pattern
+
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <memory>
+#include <string>
+
+// state interface
+class IState
+{
+    public:
+	virtual void write(class Type_Writter& type_writter, std::string what) = 0;
+	virtual ~IState() = default;
+};
+
+// context, we model a type writter with 2 states: Caps ON and Caps OFF
+// that keep switching automatically
+class Type_Writter
+{
+protected:
+    std::unique_ptr<IState> state_;
+public:
+    Type_Writter(std::unique_ptr<IState> state): state_{std::move(state)} {}
+    void set_state(std::unique_ptr<IState> state)
+    {
+        state_ = std::move(state);
+    }
+    void write(const std::string& what)
+    {
+        state_->write(*this, what);
+    }
+};
+
+// concrete states
+class Caps_ON: public IState
+{
+public:
+    void write(Type_Writter& type_writter, std::string what) override;
+};
+
+class Caps_OFF: public IState
+{
+public:
+    void write(Type_Writter& type_writter, std::string what) override;
+};
+
+// implementation of concrete states
+void Caps_ON::write(Type_Writter& type_writter, std::string what)
+{
+    std::transform(std::begin(what), std::end(what), std::begin(what), ::toupper);
+    std::cout << what;
+    type_writter.set_state(std::make_unique<Caps_OFF>()); // flip the state
+}
+
+void Caps_OFF::write(Type_Writter& type_writter, std::string what)
+{
+    std::transform(std::begin(what), std::end(what), std::begin(what), ::tolower);
+    std::cout << what;
+    type_writter.set_state(std::make_unique<Caps_ON>()); // flip the state
+}
+
+int main()
+{
+    // the context
+    Type_Writter type_writter(std::make_unique<Caps_OFF>());
+
+    // the state machine in action
+    type_writter.write("Hello ");
+    type_writter.write("World!\n");
+    type_writter.write("This ");
+    type_writter.write("is ");
+    type_writter.write("a ");
+    type_writter.write("test.\n");
+}
