f16275ec23bd3ef8aa462abed8251f329435d261
Jaimin Modi <m.jaimin@gmail.com>
2020-05-06 11:06:47 +0530

    new pattern added

---
+++ Behavioural/State/StateScheme/StateScheme.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+class State;
+class Context
+{
+    private: 
+	State* current;
+
+    public: 
+	void setState(State* state);
+	void Request();
+};
+
+class State {
+    public:
+	virtual void goNext(Context* ) = 0;
+};
+
+class StateOne: public State {
+    public:
+	void goNext(Context*);
+};
+
+class StateTwo: public State {
+    public:
+	void goNext(Context*);
+};
+
+class StateThree : public State {
+    public:
+	void goNext(Context*);
+};
+
+
+void StateOne::goNext(Context* context) {
+    std::cout<<"Current State: StateOne" << std::endl;
+    context->setState(new StateTwo());
+    std::cout<<"Next State: StateTwo\n" << std::endl;
+}
+void StateTwo::goNext(Context* context) {
+    std::cout<<"Current State: StateTwo" << std::endl;
+    context->setState(new StateThree());
+    std::cout<<"Next State: StateThree\n" << std::endl;
+}
+void StateThree::goNext(Context* context) {
+    std::cout<<"Current State: StateThree" << std::endl;
+    context->setState(new StateOne());
+    std::cout<<"Next State: StateOne\n" << std::endl;
+}
+
+
+void Context::setState(State* state) {
+    current = state;
+}
+
+void Context::Request() {
+    current->goNext(this);
+}
+
+int main()
+{
+    Context* c = new Context();
+    c->setState(new StateOne());
+    c->Request();
+    c->Request();
+    c->Request();
+    c->Request();
+    c->Request();
+}
