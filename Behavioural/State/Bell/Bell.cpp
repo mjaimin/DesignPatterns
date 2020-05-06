21340dc76d6b0fd716725551e472578dda4a83be
Jaimin Modi <m.jaimin@gmail.com>
2020-05-06 10:04:52 +0530

    new pattern added

---
+++ Behavioural/State/Bell/Bell.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+class BellState {
+    public:
+	virtual void Ring() = 0;
+};
+
+class BellRingingState : public BellState {
+    public:
+	void Ring() { printf("Bell is ringing...\n"); }
+};
+
+class BellSilentState : public BellState {
+    public:
+	void Ring() { printf("Bell isn't ringing...\n"); }
+};
+
+class Bell {
+    private:
+	BellState* currentState;
+    public:
+	Bell() { currentState = new BellRingingState(); }
+	void setState(BellState* state) { currentState = state; }
+	void alert() { currentState->Ring(); }
+};
+
+int main()
+{
+    Bell* stateContext = new Bell();
+    stateContext->alert();
+    stateContext->alert();
+    stateContext->setState(new BellSilentState());
+    stateContext->alert();
+    stateContext->alert();
+    stateContext->alert();
+}
